fbd73ca2d18dbd35d66142dff347b0e8771f2a11
kiran <kdahal385@gmail.com>
2024-05-18 22:23:58 +0545

    fff

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { AllRoutes } from "./routes/AllRoutes";
+import { Header} from "./components/Header";
+import { Footer} from "./components/Footer";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AllRoutes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
